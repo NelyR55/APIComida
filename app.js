e6604cc67081ad83ff7a04186488d551f330808d
Nely Rodriguez <nelyr844@gmail.com>
2024-06-13 18:04:50 -0600

    segundo commit

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express")
+const morgan = require ("morgan")
 const app =express()
 const port = 3500
+const comidaRouter = require('./routes/comidaRouter')
 
-app.get("/", (req,res)=>{
-    res.json({mensaje:"Aplicacion Funcionando Correctamente"})
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+
+app.use(morgan('dev'))
+
+//Rutas
+app.use('/API', comidaRouter)
+
+//Lista blanca
+app.use((req,res)=>{
+    res.status(400).json({message:'Ruta no encontrada'})
 })
 
 app.listen(port,()=>{
